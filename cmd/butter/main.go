7156e270f1ead6782ebece827161eadaf07b9e8c
cantdocpp <cihuyman95@gmail.com>
2022-07-17 18:50:40 +0700

    bot is up

--- cmd/butter/main.go
+++ cmd/butter/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+var (
+	discord *discordgo.Session
+)
+
+func init() {
+	var err error
+	discord, err = discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		fmt.Println("Bot is up!")
+	})
+
+	err := discord.Open()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer discord.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	fmt.Println("Bot is down...")
 }
